14fbc0acd2c1d1fe3661566a0c5a7f2c07c98913
Novikov Pavel <successdca@gmail.com>
2020-02-25 12:18:20 +0300

    Добавлено описание работы с FireBird и InterBase

---
+++ Work_with_SQL/FireBirdAndInterBase/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireBirdAndInterBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Подключение к FireBir.строка подключения хотя и большая но понятная
+            string connectionString = "User=SYSDBA;Password=masterkey;Database=D:\SMDK\DBase\SmarketFood.fdb;DataSource=roman-book;Port=3050;Dialect=3;Charset=WIN1251;Role=;Connection lifetime=30;Pooling=true;MinPoolSize=0;MaxPoolSize=50;Packet Size=8192;ServerType=0;";
+            FbConnection con = new FbConnection(connectionString);
+
+            con.Open();
+            FbCommand command = new FbCommand("select count(*) from table", con);
+            con.Close();
+            //FbDataReader - DataReader. (Всё как обычно работает аналогично)
+            //FbDataAdapter - DataAdapter. (Точно так-же)
+        }
+    }
+}
--- _ReadMe.txt
+++ _ReadMe.txt
@@ -1,3 +1,9 @@
 Примеры работы с SQL
 
-1. 
+1. FireBirdAndInterBase - взаимодействиe различных приложений написанных на .NET с базами данных FireBird и InterBase
+	чтобы подключиться к базе данных нам потребуется Коннектор: «FirebirdClient-2.5.0-Alpha2.msi» — возможно есть новее.
+	После установки в Program Files появится папочка FirebirdClient 2.0 — в ней лежат необходимые библиотеки.
+	Добавляем оттуда в наш проект через «Add Reference» библиотеку: FirebirdSql.Data.FirebirdClient.dll
+	FirebirdClient-2.5.0-Alpha2.msi — проверял работает как с FireBird так и с InterBase замечательно работает.
+
+2. 
